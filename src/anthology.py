ba6153ed21060bebb40f249eb0294232b3daf27a
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-08-06 11:50:47 -0500

    introduce character class

---
+++ src/anthology.py
@@ -0,0 +1,84 @@
+class Character:
+    def __init__(
+        self, name: str, age: str, gender: str, personality: str, faction: str
+    ):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.personality = personality
+        self.faction = faction
+
+
+class Anthology:
+    def __init__(
+        self,
+        name: str,
+        setting: str,
+        anthology_type: str,
+        factions: list[str],
+        characters: list[Character],
+    ) -> None:
+        self.name = name
+        self.setting = setting
+        self.anthology_type = anthology_type
+        self.factions = factions
+        self.characters = characters
+        self.year = 0
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nSetting: {self.setting}\nAnthology Type: {self.anthology_type}\nFactions: {self.factions}\nCharacters: {self.characters}"
+
+
+def generate_anthology() -> Anthology:
+    print(
+        "Welcome to Anthology GPT!\nIt's time to create your own Anthology from scratch."
+    )
+    name: str = input("Enter a name for your Anthology:\n> ")
+    print(
+        "First, let's set the stage for your Anthology. What kind of setting are you working with?"
+    )
+    setting: str = input(
+        "Example settings:\n- Fantasy\n- Victorian\n- Cyberpunk\n- Science Fiction\n> "
+    )
+    print(
+        "Now, let's design your Anthology's theme. What type of Anthology will your characters inhabit?"
+    )
+    anthology_type: str = input(
+        "Example Anthologys:\n- Endless Desert/Ocean\n- Endless Metropolis\n- Earth-like\n> "
+    )
+    print(
+        "Next, identify at least two factions you want to be involved in your Anthology."
+    )
+    factions: list[str] = []
+    while True:
+        print(
+            "Example factions:\n- Fantasy Races (Elves,Dwarves,Humans,Goblins)\n- Noble Houses/Clans/Kingdoms\n- Corporations"
+        )
+        faction = input("Enter faction name or enter nothing to end.\n> ")
+        if faction == "":
+            break
+        factions.append(faction)
+    print(
+        "Finally, provide at least two characters you want to interact as part of your stories."
+    )
+    characters: list[Character] = []
+    while True:
+        print("Enter character name or enter nothing to end.")
+        name = input("Character's Name:\n> ")
+        if name == "":
+            break
+        age = input("Character's age:\n> ")
+        gender = input("Character's gender:\n> ")
+        personality = input("Character's personality:\n> ")
+        faction = input("Character's faction\n> ")
+        characters.append(
+            Character(
+                name=name,
+                age=age,
+                gender=gender,
+                personality=personality,
+                faction=faction,
+            )
+        )
+
+    return Anthology(name, setting, anthology_type, factions, characters)
