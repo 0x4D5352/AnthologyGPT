f54207ab03cab927b65267b23bd6599170f5c918
Joshua Nussbaum <67082011+0x4D5352@users.noreply.github.com>
2024-08-07 22:07:20 -0500

    more updates

--- src/anthology.py
+++ src/anthology.py
@@ -20,39 +20,62 @@ class Character:
     def __repr__(self) -> str:
         return f"Character(name={self.name}, age={self.age}, gender={self.pronouns}, personality={self.personality}, faction={self.faction})"
 
-    def start_conversation(self, characters: Character | set[Character]) -> None:
+    def start_conversation(
+        self,
+        characters: Character | set[Character],
+        previous_messages: list[dict[str, str]],
+        current_message: str,
+    ) -> None:
         if characters not in self._conversations:
             self._conversations[characters] = [OpenAI()]
-            if isinstance(characters, set):
-                last_character = characters.pop()
-                list_of_characters = ", ".join(
-                    [
-                        f"{character.name} ({character.pronouns} pronouns)"
-                        for character in characters
-                    ]
-                )
-                list_of_characters += f", and {last_character}"
-                del last_character
-            else:
-                list_of_characters = characters.name
-            memory_of_characters = "nothing"
-            self._conversations[characters][0].add_message(
-                {
-                    "role": "system",
-                    "message": f"You are {self.name} ({self.pronouns} pronouns). You are a {self.age} year old {self.faction}. Your personality is {self.personality}. You're having a conversation with {list_of_characters}. You know this about them: {memory_of_characters}. The user is providing you withe most recent message in the conversation, and you are expected to reply in character.",
-                }
-            )
-            del list_of_characters, memory_of_characters
         else:
             self._conversations[characters].append(OpenAI())
-            conversation_number = len(self._conversations[characters])
+        if isinstance(characters, set):
+            last_character = characters.pop()
+            list_of_characters = ", ".join(
+                [
+                    f"{character.name} ({character.pronouns} pronouns)"
+                    for character in characters
+                ]
+            )
+            list_of_characters += f", and {last_character}"
+            del last_character
+        else:
+            list_of_characters = characters.name
+        memory_of_characters = "nothing"
+        self._conversations[characters][-1].add_message(
+            {
+                "role": "system",
+                "message": f"You are {self.name} ({self.pronouns} pronouns). You are a {self.age} year old {self.faction}. Your personality is {self.personality}. You're having a conversation with {list_of_characters}. You know this about them: {memory_of_characters}. The user is providing you with the most recent message in the conversation, and you are expected to reply in character.",
+            }
+        )
+        if previous_messages:
+            for message in previous_messages:
+                self._conversations[characters][-1].add_message(message)
+        del list_of_characters, memory_of_characters
+
+
+class History:
+    def __init__(self) -> None:
+        self.actual_history = None
+        self.lost_history = None
+        self.remembered_history = None
+        self.legends = None
+        self.summary = None
 
 
 class Faction:
-    def __init__(self, name: str, description: str, characters: set[Character]) -> None:
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        characters: set[Character] = set(),
+        history: History = History(),
+    ) -> None:
         self.name = name
         self.description = description
         self.characters = characters
+        self.history = history
         self._allies = None
         self._enemies = None
 
@@ -65,39 +88,31 @@ class Faction:
 
 class Era:
     def __init__(
-        self, name: str, duration: int, theme: str, factions: set[Faction]
+        self,
+        name: str,
+        duration: int,
+        theme: str,
+        factions: set[Faction] = set(),
+        history: History = History(),
     ) -> None:
         self.name = name
         self.duration = duration
         self.theme = theme
         self.factions = factions
+        self.history = history
         self._year = 0
         self._events = None
         self._conversations = None
-        self._lost_history = None
 
     def add_faction(self, faction: Faction) -> None:
         if faction in self.factions:
             return
         self.factions.add(faction)
 
-    def remove_faction(self, name: str) -> None:
-        if name not in [faction.name for faction in self.factions]:
+    def remove_faction(self, faction: Faction) -> None:
+        if faction not in self.factions:
             return
-        target = None
-        for faction in self.factions:
-            if faction.name == name:
-                target = faction
-        if target is not None:
-            self.factions.remove(target)
-
-
-class History:
-    def __init__(self) -> None:
-        self.actual_history = None
-        self.remembered_history = None
-        self.legends = None
-        self.summary = None
+        self.factions.remove(faction)
 
 
 class Anthology:
@@ -120,60 +135,3 @@ class Anthology:
             self._history = history
         else:
             self._history = History()
-
-
-def generate_anthology() -> Anthology:
-    print(
-        "Welcome to Anthology GPT!\nIt's time to create your own Anthology from scratch."
-    )
-    name: str = input("Enter a name for your Anthology:\n> ")
-    print(
-        "First, let's set the stage for your Anthology. What kind of setting are you working with?"
-    )
-    setting: str = input(
-        "Example settings:\n- Fantasy\n- Victorian\n- Cyberpunk\n- Science Fiction\n> "
-    )
-    print(
-        "Now, let's design your Anthology's \"world\". What type of Anthology will your characters inhabit?"
-    )
-    anthology_type: str = input(
-        "Example types:\n- Endless Desert/Ocean\n- Endless Metropolis\n- Earth-like\n> "
-    )
-    print(
-        "Next, identify at least two factions you want to be involved in your Anthology."
-    )
-    factions: set[Faction] = set()
-    while True:
-        print(
-            "Example factions:\n- Fantasy Races (Elves,Dwarves,Humans,Goblins)\n- Noble Houses/Clans/Kingdoms\n- Corporations"
-        )
-        faction = input("Enter faction name or enter nothing to end.\n> ")
-        if faction == "":
-            break
-        description = input("Enter faction description:\n> ")
-        factions.add(Faction(name=faction, description=description))
-    print(
-        "Finally, provide at least two characters you want to interact as part of your stories."
-    )
-    characters: list[Character] = []
-    still_generating_characters: bool = True
-    while still_generating_characters:
-        print("Enter character name or enter nothing to end.")
-        name = input("Character's Name:\n> ")
-        if name == "":
-            break
-        age = input("Character's age:\n> ")
-        gender = input("Character's gender:\n> ")
-        personality = input("Character's personality:\n> ")
-        faction = input("Character's faction\n> ")
-        characters.append(
-            Character(
-                name=name,
-                age=age,
-                pronouns=gender,
-                personality=personality,
-                faction=faction,
-            )
-        )
-
-    return Anthology(name, setting, anthology_type, factions, characters)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,79 @@
-from anthology import Anthology, generate_anthology, Character, Faction
+from anthology import Anthology, Era, Faction, Character
+
+
+def generate_setting() -> set:
+    res = set()
+    while True:
+        print(
+            "Enter a theme or genre to describe your setting, or enter nothing to stop."
+        )
+        setting: str = input(
+            "Example settings:\n- Fantasy\n- Victorian\n- Cyberpunk\n- Science Fiction\n> "
+        )
+        if setting == "":
+            break
+        res.add(setting)
+        print(f"current setting: {", ".join(res)}")
+    if len(res) == 0:
+        raise ValueError("You need at least one setting!")
+    return res
+
+
+def generate_anthology() -> Anthology:
+    print(
+        "Welcome to Anthology GPT!\nIt's time to create your own Anthology from scratch."
+    )
+    name: str = input("Enter a name for your Anthology:\n> ")
+    print(
+        "First, let's set the stage for your Anthology. What kind of setting are you working with?"
+    )
+    setting: set = generate_setting()
+    print(
+        "Now, let's design your Anthology's \"world\". What type of Anthology will your characters inhabit?"
+    )
+    anthology_type: str = input(
+        "Example types:\n- Endless Desert/Ocean\n- Endless Metropolis\n- Earth-like\n> "
+    )
+    print(
+        "Next, identify at least two factions you want to be involved in your Anthology."
+    )
+    return Anthology(name, setting, anthology_type)
+
+
+def generate_era():
+    factions: set[Faction] = set()
+    while True:
+        print(
+            "Example factions:\n- Fantasy Races (Elves,Dwarves,Humans,Goblins)\n- Noble Houses/Clans/Kingdoms\n- Corporations"
+        )
+        faction = input("Enter faction name or enter nothing to end.\n> ")
+        if faction == "":
+            break
+        description = input("Enter faction description:\n> ")
+        factions.add(Faction(name=faction, description=description))
+    print(
+        "Finally, provide at least two characters you want to interact as part of your stories."
+    )
+    characters: list[Character] = []
+    still_generating_characters: bool = True
+    while still_generating_characters:
+        print("Enter character name or enter nothing to end.")
+        name = input("Character's Name:\n> ")
+        if name == "":
+            break
+        age = input("Character's age:\n> ")
+        gender = input("Character's gender:\n> ")
+        personality = input("Character's personality:\n> ")
+        faction = input("Character's faction\n> ")
+        characters.append(
+            Character(
+                name=name,
+                age=age,
+                pronouns=gender,
+                personality=personality,
+                faction=faction,
+            )
+        )
 
 
 def main(interactive: bool = True) -> None:
@@ -7,8 +82,18 @@ def main(interactive: bool = True) -> None:
     else:
         anthology = Anthology(
             "Anthology",
-            "Fantasy",
+            {"Fantasy"},
             "Endless Desert",
+        )
+    print(anthology)
+
+
+if __name__ == "__main__":
+    main(False)
+
+
+"""
+
             {
                 Faction("Elves", "A wise and ancient race of tall, slender humanoids."),
                 Faction("Dwarves", "A short and sturdy race of miners and craftsmen."),
@@ -31,9 +116,4 @@ def main(interactive: bool = True) -> None:
                     faction="Dwarves",
                 ),
             ],
-        )
-    print(anthology)
-
-
-if __name__ == "__main__":
-    main(False)
+    """
